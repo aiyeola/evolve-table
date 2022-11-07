d7e71eef13852b99b535bfb8e5d500cbda6e45a5
Victor Aiyeola <aiyeolavictor@gmail.com>
2022-11-07 01:32:20 +0100

    chore: project set up & dependencies

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Table from "./components/Table";
+
+export default function App() {
+  return (
+    <>
+      <Table />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
